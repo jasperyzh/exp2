cd63eaf93c774f1e370d05b5a97dda07725ada5e
jaspery <jasper.yzh@gmail.com>
2023-06-19 12:00:49 +0800

    template:twintowers

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,10 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://staging.fishermen-analytics.com/",
+  base: '/exp/',
+  experimental: {
+    assets: true,
+  },
   integrations: [tailwind()],
 });
